9b1a7e5e36f4baae0666f15f46c66bcfcbd0cc50
Pedrocm99 <pedrocasasdev@gmail.com>
2024-12-10 20:21:50 +0100

    Capsule AO Mesh generation from skeleton information

--- Runtime/Rendering/SkeletonAO/SkeletonAO.cs
+++ Runtime/Rendering/SkeletonAO/SkeletonAO.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UIElements;
 
 namespace PitGL
 {
@@ -21,10 +22,15 @@ namespace PitGL
             public float radius = 0.2f;
         }
 
+        [SerializeField, ForceAsset("Runtime/Rendering/SkeletonAO/CapsuleBoneBase.fbx")] MeshFilter boneMeshFilter;
+
         [SerializeField] float minBoneLength = 0.1f;
         [SerializeField] float defaultJointRadius = 0.1f;
+        [SerializeField] Transform rigRoot;
         [SerializeField] Bone[] bones;
         [SerializeField] Joint[] joints;
+
+        private Mesh skeletonMesh;
 
         private void Start()
         {
@@ -37,6 +43,13 @@ namespace PitGL
             if (bones.Length == 0 ) return;
 
             Gizmos.color = Color.red;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            if (skeletonMesh != null)
+            {
+                Gizmos.DrawMesh(skeletonMesh);
+            }
+
+           // Gizmos.DrawMesh(boneMeshFilter.sharedMesh);
 
             Dictionary<Transform, Joint> dict = new Dictionary<Transform, Joint>();
             if(joints != null)
@@ -94,8 +107,8 @@ namespace PitGL
             }
         }
 
-        [ContextMenu("Build from hierarchy")]
-        private void BuildFromHierarchy()
+        [ContextMenu("Deduct skeleton from hierarchy")]
+        private void DeductSekeletonFromHierarchy()
         {
             List<Bone> boneList = new List<Bone>();
             Dictionary<Transform, Joint> jointsDict = new Dictionary<Transform, Joint>();
@@ -108,7 +121,7 @@ namespace PitGL
                 }
             }
 
-            Transform t = transform;
+            Transform t = rigRoot;
 
             var candidates = new Stack<(Transform, Transform)>();
 
@@ -164,7 +177,93 @@ namespace PitGL
                 joints[jointIndex++] = joint;
             }
 
+#if UNITY_EDITOR
             UnityEditor.EditorUtility.SetDirty(this);
+#endif
+        }
+
+
+        [ContextMenu("Generate mesh from skeleton")]
+        private void GenerateMeshFromSkeleton()
+        {
+            CombineInstance[] instances = new CombineInstance[bones.Length];
+
+            Mesh baseMesh = boneMeshFilter.sharedMesh;
+
+            // Initialize bone to joint dictionary
+            Dictionary<Transform, Joint> jointsDict = new Dictionary<Transform, Joint>();
+            if (joints != null)
+            {
+                for (int i = 0; i < joints.Length; i++)
+                {
+                    jointsDict.Add(joints[i].transform, joints[i]);
+                }
+            }
+
+            for(int i = 0; i < bones.Length; i++)
+            {
+                if (!jointsDict.ContainsKey(bones[i].joint0)) jointsDict.Add(bones[i].joint0, new Joint() { transform = bones[i].joint0, displacement = Vector3.zero, radius = defaultJointRadius });
+                if (!jointsDict.ContainsKey(bones[i].joint1)) jointsDict.Add(bones[i].joint1, new Joint() { transform = bones[i].joint1, displacement = Vector3.zero, radius = defaultJointRadius });
+            }
+
+            // Create bone meshes
+            for (int i = 0; i < bones.Length; i++)
+            {
+
+                Bone bone = bones[i];
+                Joint joint0 = jointsDict[bones[i].joint0];
+                Joint joint1 = jointsDict[bones[i].joint1];
+
+                Vector3[] vertices = new Vector3[baseMesh.vertices.Length];
+                Vector3[] normals = new Vector3[baseMesh.normals.Length];
+                int[] triangles = new int[baseMesh.triangles.Length];
+                baseMesh.vertices.CopyTo(vertices, 0);
+                baseMesh.normals.CopyTo(normals, 0);
+                baseMesh.triangles.CopyTo(triangles, 0);
+
+                Vector3 joint0Pos = joint0.transform.TransformPoint(joint0.displacement);
+                Vector3 joint1Pos = joint1.transform.TransformPoint(joint1.displacement);
+
+                Quaternion rot = Quaternion.LookRotation(joint1Pos - joint0Pos) * Quaternion.Euler(90f, 0f, 0f);
+
+                Matrix4x4 joint0matrix = Matrix4x4.TRS(joint0Pos, rot, Vector3.one * joint0.radius);
+                Matrix4x4 joint1matrix = Matrix4x4.TRS(joint1Pos, rot, Vector3.one * joint1.radius);
+
+                //Scale vertices to joint radiuses
+                for(int v = 0; v < vertices.Length; v++)
+                {
+                    bool isJoint0 = vertices[v].y < 0.1f;
+
+                    if (isJoint0)
+                    {
+                        vertices[v] = joint0matrix.MultiplyPoint(vertices[v]);
+                    }
+                    else
+                    {
+                        vertices[v].y -= 1f;
+                        vertices[v] = joint1matrix.MultiplyPoint(vertices[v]);
+                    }
+                    
+                    vertices[v] = transform.InverseTransformPoint(vertices[v]);
+                }
+
+                for (int n = 0; n < normals.Length; n++)
+                {
+                    normals[n] = bone.joint0.TransformDirection(normals[n]);
+                    normals[n] = transform.InverseTransformDirection(normals[n]);
+                }
+                
+                Mesh mesh = new Mesh();
+                mesh.vertices = vertices;
+                mesh.normals = normals;
+                mesh.triangles = triangles;
+                instances[i].mesh = mesh;
+            }
+
+            // Combine meshes
+            skeletonMesh = new Mesh();
+            skeletonMesh.CombineMeshes(instances, true, false, false);
+            skeletonMesh.RecalculateBounds();
         }
     }
 }
